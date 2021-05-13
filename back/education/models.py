af1d2b0d0cb31afd07db9d6d167a150d9ad51f0d
dima <d_kuznetsov@sun.rf>
2021-05-13 16:34:32 +0300

    Добавлена модель Educational_area

--- back/education/models.py
+++ back/education/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class Educational_area(models.Model):
+  name = models.TextField(max_length=200, verbose_name='Название')
+  number = models.PositiveSmallIntegerField(verbose_name='Номер')
+
+  class Meta:
+    db_table = 'educational_area'
+    verbose_name = 'Образовательная область'
+    verbose_name_plural = 'Образовательные области'
+    ordering = ['number']
+
+  def __str__(self):
+    return self.name
