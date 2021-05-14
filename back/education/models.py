9508f3b91a58a53323474df75e3c41b2fcc86137
dima <d_kuznetsov@sun.rf>
2021-05-14 09:16:18 +0300

    Добавлена модель Development_direction

--- back/education/models.py
+++ back/education/models.py
@@ -12,3 +12,22 @@ class Educational_area(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class Development_direction(models.Model):
+  area = models.ForeignKey(
+    Educational_area, null=False,
+    on_delete=models.CASCADE, verbose_name=u'Образовательная область'
+  )
+
+  name = models.TextField(max_length=200, verbose_name='Название')
+  number = models.PositiveSmallIntegerField(verbose_name='Номер')
+
+  class Meta:
+    db_table = 'development_direction'
+    verbose_name = 'Направление развития'
+    verbose_name_plural = 'Направления развития'
+    ordering = ['number']
+
+  def __str__(self):
+    return self.name
