fdfa9b1e2000a2362de353dbdc826662f880884c
vova <12345678>
2021-05-16 14:50:16 +0300

    Api auth

--- front/src/store/auth.js
+++ front/src/store/auth.js
@@ -17,15 +17,62 @@ const getters = {
 
 const actions = {
   async login({ commit }) {
-    commit("setIsAuth", true);
-    commit("setUserInfo", {
-      name: 'Иванов И.И.',
-      staff: true,
-    });
+    try {
+      let res = await this.$axios.post("/api/login/", payload);
+      if (res.status === 200) {
+        commit("setIsAuth", true);
+        commit("setUserInfo", {
+          name: res.data.fullname,
+          staff: res.data.is_staff,
+        });
+      }
+      return res;
+    } catch (e) {
+      commit("setIsAuth", false);
+      commit("setUserInfo", { name: null, staff: false, });
+      if (e.response) {
+        return e.response;
+      } else {
+        return e;
+      }
+    }
   },
   async logout({ commit }) {
-    commit("setIsAuth", false);
-    commit("setUserInfo", { name: null, staff: false, unit_id: null });
+    try {
+      let res = await this.$axios.post("/api/logout/");
+      if (res.status === 200) {
+        commit("setIsAuth", false);
+        commit("setUserInfo", { name: null, staff: false });
+      }
+      return res;
+    } catch (e) {
+      if (e.response) {
+        return e.response;
+      } else {
+        return e;
+      }
+    }
+  },
+  async checkUser({ commit }) {
+    try {
+      let res = await this.$axios.get("/api/user/?current=true");
+      if (res.status === 200) {
+        commit("setIsAuth", true);
+        commit("setUserInfo", {
+          name: res.data.results[0].fullname,
+          staff: res.data.results[0].is_staff,
+        });
+      }
+      return res;
+    } catch (e) {
+      commit("setIsAuth", false);
+      commit("setUserInfo", { name: null, staff: false });
+      if (e.response) {
+        return e.response;
+      } else {
+        return e;
+      }
+    }
   },
 };
 
