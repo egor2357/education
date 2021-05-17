9551e5bc6b52059e37b05bb494f0f05cde6372cb
dima <d_kuznetsov@sun.rf>
2021-05-17 10:33:16 +0300

    Добавлены модели в админку:
    Educational_area, Development_direction, Skill

--- back/education/admin.py
+++ back/education/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Educational_area, Development_direction, Skill
+
 # Register your models here.
+class Educational_areaAdmin(admin.ModelAdmin):
+  search_fields = ('name', 'number')
+  list_display = ('number', 'name')
+
+admin.site.register(Educational_area, Educational_areaAdmin)
+
+class Development_directionAdmin(admin.ModelAdmin):
+  search_fields = ('name', 'number')
+  list_display = ('number', 'name', 'area')
+  list_filter = ('area',)
+
+admin.site.register(Development_direction, Development_directionAdmin)
+
+class SkillAdmin(admin.ModelAdmin):
+  search_fields = ('name', 'number')
+  list_display = ('number', 'name', 'direction')
+  list_filter = ('direction',)
+
+admin.site.register(Skill, SkillAdmin)
