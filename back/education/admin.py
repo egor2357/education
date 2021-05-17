2869615ad7f24ce72c471060601d6a853d060cb7
dima <d_kuznetsov@sun.rf>
2021-05-17 11:33:33 +0300

    Добавлена модель в админку:
    Presense

--- back/education/admin.py
+++ back/education/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Educational_area, Development_direction, Skill, Form, Method
-from .models import Specialist, Competence, Specialty
+from .models import Specialist, Competence, Specialty, Presense
 from .models import Activity, Schedule
 
 # Register your models here.
@@ -64,6 +64,12 @@ class SpecialistAdmin(admin.ModelAdmin):
   inlines = (CompetenceInline, SpecialtyInline)
 
 admin.site.register(Specialist, SpecialistAdmin)
+
+class PresenseAdmin(admin.ModelAdmin):
+  list_filter = ('specialist', 'is_available')
+  list_display = ('specialist', 'date_from', 'date_to', 'is_available')
+
+admin.site.register(Presense, PresenseAdmin)
 
 class ScheduleInline(admin.TabularInline):
   model = Schedule
