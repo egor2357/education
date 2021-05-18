c6a3716fb1356b9cd8ebf1372af4aef4576cc664
dima <d_kuznetsov@sun.rf>
2021-05-18 11:43:28 +0300

    Добавлены пути для users, login, logout, current

--- back/education/urls.py
+++ back/education/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from .views import *
 
 router = routers.DefaultRouter()
+router.register(r'users', UserView)
 
 urlpatterns = [
   path('', include(router.urls)),
+  path('login/', LoginView.as_view()),
+  path('logout/', LogoutView.as_view()),
+  path('current/', CurrentUserView.as_view()),
+
+
   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
