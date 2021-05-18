522b4a8068c46d023b13fb73dad514bbe1b9684f
dima <d_kuznetsov@sun.rf>
2021-05-18 11:37:45 +0300

    Добавлен класс аутентификации без проверки csrf

--- back/education/views.py
+++ back/education/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+  def enforce_csrf(self, request):
+    return
 
 # Create your views here.
