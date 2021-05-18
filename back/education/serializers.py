d1a5d4a3f27ada50fe01f49a53185af93926d907
dima <d_kuznetsov@sun.rf>
2021-05-18 11:35:35 +0300

    Добавлен LoginSerializer

--- back/education/serializers.py
+++ back/education/serializers.py
@@ -1,6 +1,36 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from .models import *
 
 #create your serializers here.
+class LoginSerializer(serializers.Serializer):
+  username = serializers.CharField()
+  password = serializers.CharField()
+
+  def validate(self, data):
+    username = data.get('username', None)
+    password = data.get('password', None)
+
+    if username is None:
+      raise serializers.ValidationError(
+        'Логин пользователя не введен'
+      )
+    if password is None:
+      raise serializers.ValidationError(
+        'Пароль пользователя не введен'
+      )
+
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+      raise serializers.ValidationError(
+        'Логин или пароль пользователя не верен'
+      )
+    if not user.is_active:
+      raise serializers.ValidationError(
+        'Пользователь не активен'
+      )
+
+    return {'user': user}
