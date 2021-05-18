b9202fa863d609c8428f4140cb3a8f17edc331cb
dima <d_kuznetsov@sun.rf>
2021-05-18 11:41:45 +0300

    Добавлен LoginView

--- back/education/views.py
+++ back/education/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets, views, permissions
+from rest_framework.response import Response
+
+from django.contrib.auth import login, logout
+
+from .models import *
+from .serializers import *
+
 from rest_framework.authentication import SessionAuthentication
 class CsrfExemptSessionAuthentication(SessionAuthentication):
   def enforce_csrf(self, request):
     return
 
 # Create your views here.
+class LoginView(views.APIView):
+  permission_classes = (permissions.AllowAny,)
+  authentication_classes = (CsrfExemptSessionAuthentication,)
+  serializer_class = LoginSerializer
+
+  def post(self, request):
+    serializer = self.serializer_class(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    login(request, user)
+    return Response(UserSerializer(user).data)
