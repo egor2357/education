c4d171625a61131664fdef81da33695c59677fe2
dima <d_kuznetsov@sun.rf>
2021-05-18 11:07:24 +0300

    Добавлена модель в админку:
    Session

--- back/education/admin.py
+++ back/education/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.sessions.models import Session
 from .models import *
+
+class SessionAdmin(admin.ModelAdmin):
+  def _session_data(self, obj):
+    return obj.get_decoded()
+  list_display = ['session_key', '_session_data', 'expire_date']
+admin.site.register(Session, SessionAdmin)
 
 # Register your models here.
 class Educational_areaAdmin(admin.ModelAdmin):
