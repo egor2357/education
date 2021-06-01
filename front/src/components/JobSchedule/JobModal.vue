fa3374df142193277696c1eb16e44207bb2caaa5
dima <d_kuznetsov@sun.rf>
2021-06-01 15:11:50 +0300

    Доделал создание занятий в модальном окне

--- front/src/components/JobSchedule/JobModal.vue
+++ front/src/components/JobSchedule/JobModal.vue
@@ -4,24 +4,27 @@
     cancelText="Отмена"
     :visible="true"
     :title="title"
-    @cancel="$emit('closeModal')"
+    @cancel="closeModal(false)"
     @ok="handleOk"
     @confirmLoading="loadingButton">
     <a-form-model :model="form" v-bind="layout" :rules="rules" ref="form">
 
       <a-form-model-item prop="date" label="Дата" key="date"
-        validateStatus="" help="">
+        :validateStatus="fields['date'].validateStatus" :help="fields['date'].help">
         <a-date-picker
           v-model="form.date"
           type="date"
           placeholder="Выберите дату"
+          format="DD.MM.YYYY"
           style="width: 100%;"
+          @change="fieldChanged('date')"
         />
       </a-form-model-item>
 
       <a-form-model-item prop="activity_id" label="Вид деятельности" key="activity_id"
-        validateStatus="" help="">
-        <a-select v-model="form.activity_id" @change="form.specialist_id=null">
+        :validateStatus="fields['activity_id'].validateStatus" :help="fields['activity_id'].help">
+        <a-select v-model="form.activity_id"
+          @change="form.specialist_id=null; fieldChanged('activity_id')">
           <a-select-option v-for="activity in activities" :key="activity.id">
             {{ activity.name }}
           </a-select-option>
@@ -29,8 +32,9 @@
       </a-form-model-item>
 
       <a-form-model-item prop="specialist_id" label="Специалист" key="specialist_id"
-        validateStatus="" help="">
-        <a-select v-model="form.specialist_id">
+        :validateStatus="fields['specialist_id'].validateStatus" :help="fields['specialist_id'].help">
+        <a-select v-model="form.specialist_id"
+          @change="fieldChanged('specialist_id')">
           <a-select-option v-for="specialist in filteredSpecialists" :key="specialist.id">
             {{ specialist.__str__ }}
           </a-select-option>
@@ -38,10 +42,11 @@
       </a-form-model-item>
 
       <a-form-model-item prop="start_time" label="Время" key="start_time"
-        validateStatus="" help="">
+        :validateStatus="fields['start_time'].validateStatus" :help="fields['start_time'].help">
         <a-input
           v-model="form.start_time"
           type="time"
+          @change="fieldChanged('start_time')"
         />
       </a-form-model-item>
 
@@ -52,6 +57,9 @@
 
 <script>
 import consts from "@/const";
+import post from "@/middleware/post";
+import put from "@/middleware/put";
+
 export default {
   name: "ModalTemplateJob",
   props: {
@@ -81,6 +89,24 @@ export default {
       layout: {
         labelCol: { span: 8 },
         wrapperCol: { span: 16 },
+      },
+      fields: {
+        'date': {
+          validateStatus: "",
+          help: "",
+        },
+        'activity_id': {
+          validateStatus: "",
+          help: "",
+        },
+        'specialist_id': {
+          validateStatus: "",
+          help: "",
+        },
+        'start_time': {
+          validateStatus: "",
+          help: "",
+        },
       },
       rules: {
         date: [
@@ -139,46 +165,50 @@ export default {
     }
   },
   methods: {
+    fieldChanged(field_key) {
+      this.fields[field_key].validateStatus = "";
+      this.fields[field_key].help = "";
+    },
+
+    closeModal(isSuccess){
+      this.$emit('closeModal', isSuccess);
+    },
+
     async handleOk() {
       this.loadingButton = true;
       this.$refs.form.validate(async (valid) => {
         if (valid) {
-          // let dispatchName = "";
-          // let successCode = 0;
-          // let successMessage = "";
+          try {
+            let successCode = 0;
+            let res = null;
+            let successMessage = "";
+            if (this.form.id) {
+              successCode = 200;
+              successMessage = "Занятие успешно изменено";
+              res = await put(this.$axios, `/api/jobs/${this.form.id}/`, this.form);
+            } else {
+              successCode = 201;
+              successMessage = "Занятие успешно добавлено";
+              res = await post(this.$axios, "/api/jobs/", this.form);
+            }
 
-          // if (this.adding) {
-          //   dispatchName = "schedule/addJob";
-          //   successCode = 201;
-          //   successMessage = "Занятие успешно добавлено";
-          // } else if (!this.adding) {
-          //   dispatchName = "schedule/editJob";
-          //   successCode = 200;
-          //   successMessage = "Занятие успешно изменено";
-          // }
-          // try {
-          //   let res = await this.$store.dispatch(dispatchName, this.form);
-          //   if (res.status === successCode) {
-          //     this.$message.success(successMessage);
-          //     this.$emit("closeModal");
-          //   } else if (res.status === 400) {
-          //     this.$message.error("Проверьте введённые данные");
-          //     for (let key in res.data) {
-          //       this.fields.forEach((field) => {
-          //         if (field.name === key) {
-          //           field.validateStatus = "error";
-          //           field.help = res.data[key];
-          //         }
-          //       });
-          //     }
-          //   } else {
-          //     this.$message.error("Произошла ошибка");
-          //   }
-          // } catch (e) {
-          //   this.$message.error("Произошла ошибка");
-          // } finally {
-          //   this.loadingButton = false;
-          // }
+            if (res.status === successCode) {
+              this.$message.success(successMessage);
+              this.closeModal(true);
+            } else if (res.status === 400) {
+              this.$message.error("Проверьте введённые данные");
+              for (let key in res.data) {
+                this.fields[key].validateStatus = "error";
+                this.fields[key].help = res.data[key];
+              }
+            } else {
+              this.$message.error("Произошла ошибка");
+            }
+          } catch (e) {
+            this.$message.error("Произошла ошибка");
+          } finally {
+            this.loadingButton = false;
+          }
         } else {
           return false;
         }
