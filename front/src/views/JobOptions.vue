900c4ec8503de3158650539da5ea2f726afa52e7
dima <d_kuznetsov@sun.rf>
2021-06-17 09:54:20 +0300

    Добавлены табы в планы занятий

--- front/src/views/JobOptions.vue
+++ front/src/views/JobOptions.vue
@@ -2,6 +2,15 @@
   <a-spin :spinning="loading">
     <div class="job-options">
       <div class="job-options__header">Планы занятий</div>
+      <div class="job-options__tabs">
+        <a-tabs v-model="activeTab">
+          <a-tab-pane v-for="activity in activities" :key="activity.id">
+            <span slot="tab">
+              {{ activity.name }}
+            </span>
+          </a-tab-pane>
+        </a-tabs>
+      </div>
 
     </div>
   </a-spin>
@@ -18,18 +27,24 @@ export default {
     return {
       loading: true,
 
+      activeTab: null,
+
     };
   },
   async created() {
     let fetches = []
 
-    // if (!this.areasFetched) {
-    //   fetches.push(this.fetchAreas());
-    // }
+    if (!this.activitiesFetched) {
+      fetches.push(this.fetchActivities());
+    }
 
     this.loading = true;
     await Promise.all(fetches);
     this.loading = false;
+
+    if (this.activities.length) {
+      this.activeTab = this.activities[0].id;
+    }
 
   },
   methods: {
@@ -52,13 +67,13 @@ export default {
     // },
 
     ...mapActions({
-      // fetchAreas: "skills/fetchAreas",
+      fetchActivities: "activities/fetchActivities",
     }),
   },
   computed: {
     ...mapGetters({
-    //   areasFetched: "skills/getFetched",
-    //   areas: "skills/getFilteredAreas",
+      activitiesFetched: "activities/getFetched",
+      activities: "activities/getActivities",
     }),
   },
 };
@@ -74,4 +89,7 @@ export default {
       text-align: center
       font-size: 16px
       margin-bottom: 10px
+    &__tabs
+      display: flex
+      justify-content: center
 </style>
