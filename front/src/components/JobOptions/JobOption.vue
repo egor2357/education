9c1a745a5889a7fcd68260dc59a3dba3f63ed6aa
vova <12345678>
2021-06-25 11:00:27 +0300

    Add point in skills in JobOption

--- front/src/components/JobOptions/JobOption.vue
+++ front/src/components/JobOptions/JobOption.vue
@@ -16,7 +16,7 @@
           >
             {{ skill.area_number }}.{{ skill.direction_number }}.{{
               skill.number
-            }}
+            }}.
             {{ skill.name }}
           </div>
         </template>
