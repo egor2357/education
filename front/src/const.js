3624a336eb5f07b88e3b4d0edc14d9b80916c4e9
vova <12345678>
2021-06-29 16:15:06 +0300

    Add thumbs for photo gallery, delete avi from videoFormats

--- front/src/const.js
+++ front/src/const.js
@@ -1,15 +1,15 @@
 const daysOfWeek = [
-  { short: "Пн", long: "Понедельник",},
-  { short: "Вт", long: "Вторник",},
-  { short: "Ср", long: "Среда",},
-  { short: "Чт", long: "Четверг",},
-  { short: "Пт", long: "Пятница",},
-  { short: "Сб", long: "Суббота",},
-  { short: "Вс", long: "Воскресенье",},
+  { short: "Пн", long: "Понедельник" },
+  { short: "Вт", long: "Вторник" },
+  { short: "Ср", long: "Среда" },
+  { short: "Чт", long: "Четверг" },
+  { short: "Пт", long: "Пятница" },
+  { short: "Сб", long: "Суббота" },
+  { short: "Вс", long: "Воскресенье" },
 ];
 
 export default {
   daysOfWeek: daysOfWeek,
-  photoFormats: ['jpg', 'png'],
-  videoFormats: ['mp4', 'avi'],
+  photoFormats: ["jpg", "png"],
+  videoFormats: ["mp4"],
 };
