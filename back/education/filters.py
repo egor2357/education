479af7631f110421d105f231916945ebc8b9cb0c
dima <d_kuznetsov@sun.rf>
2021-08-13 11:57:40 +0300

    Пагинация изменена на CommonPagination
    Для использования в большинстве моделей

--- back/education/filters.py
+++ back/education/filters.py
@@ -7,6 +7,29 @@ from .views import *
 from .models import *
 from .serializers import *
 
+class CommonPagination(PageNumberPagination):
+  page_size = 10
+  page_size_query_param = 'per_page'
+  max_page_size = 1000
+
+  def get_paginated_response(self, data):
+    page_size = self.get_page_size(self.request)
+    if page_size is None:
+      page_size = 10
+    return Response({
+      'pagination': {
+        'count': self.page.paginator.count,
+        'page': self.page.number,
+        'per_page': page_size,
+        'links': {
+          'next': self.get_next_link(),
+          'previous': self.get_previous_link()
+        },
+      },
+      'results': data
+    })
+
+
 class JobFilter(FilterSet):
   class Meta:
     model = Job
@@ -86,27 +109,7 @@ class OptionFilter(FilterSet):
       'activity_id',
     ]
 
-class MissionPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'per_page'
-    max_page_size = 1000
 
-    def get_paginated_response(self, data):
-      page_size = self.get_page_size(self.request)
-      if page_size is None:
-        page_size = 10
-      return Response({
-        'pagination': {
-          'count': self.page.paginator.count,
-          'page': self.page.number,
-          'per_page': page_size,
-          'links': {
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link()
-          },
-        },
-        'results': data
-      })
 
 class MissionFilter(FilterSet):
   caption = django_filters.CharFilter(
