7652283e9cc8f5ba31ef87e74007cf38ee94a830
vova <12345678>
2021-08-19 15:12:02 +0300

    Add sort for TaskGroups

--- back/education/views.py
+++ back/education/views.py
@@ -727,10 +727,11 @@ class MessageView(CreateListRetrieveDestroyViewSet):
 class Task_groupView(viewsets.ModelViewSet):
   authentication_classes = (CsrfExemptSessionAuthentication,)
   permission_classes = (permissions.IsAuthenticated, IsAdminOrReadCreateOnly)
-  filter_backends = (DjangoFilterBackend,)
+  filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
   queryset = Task_group.objects.all()
   filterset_class = Task_groupFilter
   pagination_class = CommonPagination
+  ordering_fields = ['creation_date', 'deadline']
 
   def perform_create(self, serializer):
     user = self.request.user
