6a9b1777b18d1dcddb3e08be68822aa3e41309bf
dima <d_kuznetsov@sun.rf>
2022-10-12 13:47:01 +0300

    Сортировка и поиск вариантов занятий на фронте в планах занятий специалиста для админа

--- front/src/components/Specialists/JobOptionsTabs.vue
+++ front/src/components/Specialists/JobOptionsTabs.vue
@@ -32,6 +32,24 @@
             </a-tab-pane>
           </a-tabs>
         </div>
+        <div class="job-options__filters-container">
+          <a-input
+            v-model="search"
+            @change="searchOptions"
+            class="job-options__filters-search"
+            placeholder="Поиск по карточкам"
+          />
+          <a-select
+            v-model="orderingType"
+            @change="fetchOptions"
+            class="job-options__filters-select"
+          >
+            <a-select-option value="-date">По дате добавления <a-icon type="caret-down" /></a-select-option>
+            <a-select-option value="date">По дате добавления <a-icon type="caret-up" /></a-select-option>
+            <a-select-option value="topic">По алфавиту <a-icon type="sort-ascending" /></a-select-option>
+            <a-select-option value="-topic">По алфавиту <a-icon type="sort-descending" /></a-select-option>
+          </a-select>
+        </div>
         <div class="job-options__cards" v-if="currentActivityOptions.length">
           <div
             class="job-options__card"
@@ -69,49 +87,11 @@ export default {
       loading: true,
       activeTab: null,
       options: [],
+
+      search: '',
+      searchTimeoutId: 0,
+      orderingType: '-date',
     };
-  },
-  beforeCreate() {
-    this.simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
-  },
-  async created() {
-    let fetches = [];
-
-    if (!this.activitiesFetched) {
-      fetches.push(this.fetchActivities());
-    }
-
-    fetches.push(this.fetchOptions());
-
-    this.loading = true;
-    await Promise.all(fetches);
-    this.loading = false;
-
-    if (this.filteredActivities.length) {
-      this.activeTab = this.filteredActivities[0].id;
-    }
-  },
-  methods: {
-    async fetchOptions() {
-      try {
-        this.loading = true;
-        let res = await this.$axios.get(
-          `/api/options/?specialist_id=${this.currentUser.id}`
-        );
-        if (res.status === 200) {
-          this.options = res.data;
-        } else {
-          this.$message.error("Ошибка при загрузке планов занятий специалиста");
-        }
-      } catch (e) {
-        this.$message.error("Ошибка при загрузке планов занятий специалиста");
-      } finally {
-        this.loading = false;
-      }
-    },
-    ...mapActions({
-      fetchActivities: "activities/fetchActivities",
-    }),
   },
   computed: {
     ...mapGetters({
@@ -135,6 +115,60 @@ export default {
       return this.filteredActivities.find((activity) => {
         return activity.id == this.activeTab;
       });
+    },
+  },
+  beforeCreate() {
+    this.simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
+  },
+  async created() {
+    let fetches = [];
+
+    if (!this.activitiesFetched) {
+      fetches.push(this.fetchActivities());
+    }
+
+    fetches.push(this.fetchOptions());
+
+    this.loading = true;
+    await Promise.all(fetches);
+    this.loading = false;
+
+    if (this.filteredActivities.length) {
+      this.activeTab = this.filteredActivities[0].id;
+    }
+  },
+  methods: {
+    ...mapActions({
+      fetchActivities: "activities/fetchActivities",
+    }),
+
+    async fetchOptions() {
+      try {
+        this.loading = true;
+        let queryParams = `?specialist_id=${this.currentUser.id}&ordering=${this.orderingType}`;
+        if (this.search) {
+          queryParams += `&search=${this.search}`
+        }
+        let res = await this.$axios.get(
+          `/api/options/${queryParams}`
+        );
+        if (res.status === 200) {
+          this.options = res.data;
+        } else {
+          this.$message.error("Ошибка при загрузке планов занятий специалиста");
+        }
+      } catch (e) {
+        this.$message.error("Ошибка при загрузке планов занятий специалиста");
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    searchOptions() {
+      if (this.searchTimeoutId) {
+        clearTimeout(this.searchTimeoutId);
+      }
+      this.searchTimeoutId = setTimeout(this.fetchOptions, 400);
     },
   },
 };
@@ -175,11 +209,23 @@ export default {
     flex-direction: column
     flex: 1
     overflow-y: auto
-    margin-top: 10px    
+    margin-top: 10px
 
   &__card
     position: relative
     padding: 3px
     width: 900px
     margin: 0 auto
+  &__filters-container
+    display: flex
+    flex-direction: row
+    align-items: center
+    width: 900px
+    margin: 10px auto 0
+    padding: 3px
+  &__filters-search
+    margin-right: 10px
+    flex: 2
+  &__filters-select
+    flex: 1
 </style>
