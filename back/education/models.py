cad0f6e3d80c2ec338e6aaac149b57aa7bb0ba5f
Dima <Lutisuperstar@mail.ru>
2025-01-22 15:25:54 +0300

    Связь упражнений и специалистов

--- back/education/models.py
+++ back/education/models.py
@@ -146,6 +146,7 @@ class Specialist(models.Model):
   )
 
   activities = models.ManyToManyField('Activity', through='Specialty', verbose_name='Направления деятельности')
+  exercises = models.ManyToManyField(Exercise, verbose_name='Разрешенные упражнения')
 
   surname = models.CharField(max_length=50, blank=True, default='', verbose_name='Фамилия')
   name = models.CharField(max_length=50, blank=True, default='', verbose_name='Имя')
