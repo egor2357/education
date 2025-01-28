15b86ec4d91102cb0bf103adfbae46b130b93afa
egor <egor2357@yandex.ru>
2025-01-28 09:51:45 +0300

    Переход на разработку в Центре

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   devServer: {
-    host: '82.146.55.188',
+    host: '192.168.64.100',
     proxy: {
       '/api': {
-        target: 'http://82.146.55.188:9898',
+        target: 'http://192.168.64.100:9898',
         // target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
