b44c61314856d0c90ee6e3c4d2c26cf635eb80c6
dima <d_kuznetsov@sun.rf>
2025-02-11 15:05:52 +0300

    Экшн by_specialist

--- back/education/views/educational_areas.py
+++ back/education/views/educational_areas.py
@@ -46,6 +46,39 @@ def get_prefetched_structure(param_filter, queryset):
   )
   return areas
 
+def get_filtered_structure_by_specialist(param_filter, specialist):
+  exercises_of_specialist = specialist.exercises.filter(param_filter)
+  results = Result.objects.filter(param_filter, pk__in=exercises_of_specialist.values_list('result_id', flat=True))
+  skills = Skill.objects.filter(param_filter, pk__in=results.values_list('skill_id', flat=True))
+  directions = Development_direction.objects.filter(param_filter, pk__in=skills.values_list('direction_id', flat=True))
+  areas = Educational_area.objects.filter(param_filter, pk__in=directions.values_list('area_id', flat=True))
+
+  directions_prefetch = Prefetch(
+    'development_direction_set',
+    queryset=directions,
+    to_attr='development_direction_filtered'
+  )
+  skills_prefetch = Prefetch(
+    'development_direction_filtered__skill_set',
+    queryset=skills,
+    to_attr='skill_filtered'
+  )
+  results_prefetch = Prefetch(
+    'development_direction_filtered__skill_filtered__result_set',
+    queryset=results,
+    to_attr='result_filtered'
+  )
+  exercises_prefetch = Prefetch(
+    'development_direction_filtered__skill_filtered__result_filtered__exercises',
+    queryset=exercises_of_specialist,
+    to_attr='exercises_filtered'
+  )
+
+  areas = areas.prefetch_related(
+    directions_prefetch, skills_prefetch, results_prefetch, exercises_prefetch
+  )
+  return areas
+
 def check_if_deleted_by_date(lifetime, by_date):
   return bool(lifetime) and bool(lifetime.upper) and (lifetime.upper <= by_date)
 def get_serialized_by_date_structure(areas, by_date):
@@ -96,6 +129,54 @@ def get_serialized_by_date_structure(areas, by_date):
 
   return response_data
 
+def get_filtered_serialized_structure(areas):
+  response_data = [{
+    'id': area.pk,
+    'name': area.name,
+    'number': area.number,
+    'deleted': False,
+    'development_directions': [{
+      'id': direction.pk,
+      'area_id': area.pk,
+      'name': direction.name,
+      'number': direction.number,
+      'area_number': area.number,
+      'deleted': False,
+      'skills': [{
+        'id': skill.pk,
+        'direction_id': direction.pk,
+        'area_number': area.number,
+        'direction_number': direction.number,
+        'name': skill.name,
+        'number': skill.number,
+        'deleted': False,
+        'results': [{
+          'id': result.pk,
+          'skill_id': skill.pk,
+          'area_number': area.number,
+          'direction_number': direction.number,
+          'skill_number': skill.number,
+          'name': result.name,
+          'number': result.number,
+          'deleted': False,
+          'exercises': [{
+            'id': exercise.pk,
+            'result_id': result.pk,
+            'area_number': area.number,
+            'direction_number': direction.number,
+            'skill_number': skill.number,
+            'result_number': result.number,
+            'name': exercise.name,
+            'number': exercise.number,
+            'deleted': False,
+          } for exercise in result.exercises_filtered]
+        } for result in skill.result_filtered]
+      } for skill in direction.skill_filtered]
+    } for direction in area.development_direction_filtered]
+  } for area in areas]
+
+  return response_data
+
 class Educational_areaView(viewsets.ModelViewSet):
   permission_classes = (permissions.IsAuthenticated, IsAdminOrReadOnly)
   serializer_class = Educational_areaSerializer
@@ -106,6 +187,8 @@ class Educational_areaView(viewsets.ModelViewSet):
     if self.action == 'by_date':
       return Educational_area.objects.all()
     if self.action == 'by_interval':
+      return Educational_area.objects.all()
+    if self.action == 'by_specialist':
       return Educational_area.objects.all()
     else:
       return getEducational_areaQueryset(self.request)
@@ -189,6 +272,26 @@ class Educational_areaView(viewsets.ModelViewSet):
 
     areas = get_prefetched_structure(param_filter, queryset)
     response_data = get_serialized_by_date_structure(areas, end)
+
+    return Response(response_data, status=status.HTTP_200_OK)
+
+  @action(
+    detail=False, methods=['get'],
+    permission_classes=(permissions.IsAuthenticated,),
+  )
+  def by_specialist(self, request):
+    specialist = request.user.specialist
+    queryset = self.get_queryset()
+
+    if request.user.is_staff:
+      by_date = datetime.date.today()
+      param_filter = Q(lifetime__lower__lte=by_date) & ~Q(Q(lifetime__upper_inf=False) & Q(lifetime__upper__lte=by_date))
+      areas = get_prefetched_structure(param_filter, queryset)
+      response_data = get_serialized_by_date_structure(areas, by_date)
+    else:
+      param_filter = Q(lifetime__upper_inf=True)
+      areas = get_filtered_structure_by_specialist(param_filter, specialist)
+      response_data = get_filtered_serialized_structure(areas)
 
     return Response(response_data, status=status.HTTP_200_OK)
 
